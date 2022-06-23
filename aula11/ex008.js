a75b9fafbfecfc257dfc311e05d4b0ad4bc74910
Eduardo Lopes <64793291+duh-lopes@users.noreply.github.com>
2022-06-23 16:39:43 -0300

    condicoes

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+
+
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) { 
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+} else {
+    console.log('Diriga sempre usando cinto de segunraça!')
+}
